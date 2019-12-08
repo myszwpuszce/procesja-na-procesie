55803bc0a078e8456571bb4ab434239b81005cfb
root <root@vagrant.vm>
2019-12-08 15:50:01 +0000

    Sun Dec  8 15:50:01 UTC 2019

--- jenkins_build.sh
+++
@@ -1,2 +0,0 @@
-#/usr/bin/java -jar /opt/jenkins-cli.jar -s http://80.48.101.133:8080 $1 $2 $3 $4 $5 $6 $7 $8 $9
-/usr/bin/java -jar /opt/jenkins-cli.jar -auth admin:admin -s http://80.48.101.133:8080 build 'Test'
